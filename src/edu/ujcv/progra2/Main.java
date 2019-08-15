c02c6bd1b1f978ce8e2e2af15c8dd9b8733f48e6
DanielB117 <daniel.betancourth@ujcv.edu.hn>
2019-08-14 21:02:58 -0600

    trabajando en el menu

--- src/edu/ujcv/progra2/Main.java
+++ src/edu/ujcv/progra2/Main.java
@@ -1,8 +1,14 @@
 package edu.ujcv.progra2;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int opcion=0;
+        pila Pila = new pila();
+        do{
+            System.out.println("Que desea hacer");
+        }
 
     }
 
