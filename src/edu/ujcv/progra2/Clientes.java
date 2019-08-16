325cad20d62415cf7026ddfbd1dc5e4a05ab2244
DanielB117 <daniel.betancourth@ujcv.edu.hn>
2019-08-15 18:12:33 -0600

    terminando el dimarray y empezando la factura, reiniciando el menu

--- src/edu/ujcv/progra2/Clientes.java
+++ src/edu/ujcv/progra2/Clientes.java
@@ -1,4 +1,35 @@
 package edu.ujcv.progra2;
 
 public class Clientes {
+    private String NombreDeCliente;
+    private float NumeroDeCuenta;
+
+
+
+    public Clientes(String NombreDeCliente, float NumeroDeCuenta) {
+        this.NombreDeCliente = NombreDeCliente;
+        this.NumeroDeCuenta = NumeroDeCuenta;
+    }
+
+
+    public String getNombreDeCliente() {
+        return NombreDeCliente;
+    }
+
+    public void setNombreDeCliente() {
+        this.NumeroDeCuenta = NumeroDeCuenta;
+    }
+
+    public float getNumeroDeCuenta() {
+        return NumeroDeCuenta;
+    }
+
+    public void setNumeroDeCuenta(float NumeroDeCuenta) {
+        this.NumeroDeCuenta = NumeroDeCuenta;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %f",NombreDeCliente,NumeroDeCuenta);
+    }
 }
